4cefb2cbd8e7f80a1245a6e61bf971f01a64a242
altrdev <alessandro.travi@gmail.com>
2025-04-04 08:57:55 +0200

    feat: new delivr

--- lovelace-nonow-aqi.js
+++ lovelace-nonow-aqi.js
@@ -3,8 +3,8 @@ import {
   html,
   LitElement,
 } from 'https://esm.run/lit-element@2.0.1/lit-element.js';
-import dayjs from 'https://cdn.jsdelivr.net/npm/dayjs@1.11.13/dayjs.min.js';
-import localizedFormat from "https://cdn.jsdelivr.net/npm/dayjs@1.11.13/esm/plugin/localizedFormat/index.js";
+import dayjs from 'https://cdn.jsdelivr.net/npm/dayjs@1.11.13/+esm';
+import localizedFormat from "https://cdn.jsdelivr.net/npm/dayjs@1.11.13/esm/plugin/localizedFormat/index.js/+esm";
 
 dayjs.extend(localizedFormat);
 
